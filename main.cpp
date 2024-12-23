3df34832c2f3f1892afe09e36488c5e0b8f23396
Mahros EL-Qabasy <mahros.elqabasy@hotmail.com>
2024-12-23 11:48:14 +0200

    Initial Project Structure

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include "Table.hpp"
+
+// main
+int main(int argc, char const *argv[]){
+    // Here is usage example of using classic table
+    
+    // init table here
+    Table::Table<int> table;
+    table.insert(150);
+
+
+    /* code */
+    return EXIT_SUCCESS;
+}
